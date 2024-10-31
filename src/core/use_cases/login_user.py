697e86ec41020e5636502f8c4f346bb9a3430cad
artempelevin <artempelevin@list.ru>
2024-10-31 19:47:21 +0300

    refactor: make use_cases methods as async

--- src/core/use_cases/login_user.py
+++ src/core/use_cases/login_user.py
@@ -8,8 +8,8 @@ class LoginUserUseCase:
         self._user_repo = user_service
         self._password_service = password_service
 
-    def login(self, login: str, password: str) -> User:
-        user = self._user_repo.find_by_login(login=login)
+    async def login(self, login: str, password: str) -> User:
+        user = await self._user_repo.find_by_login(login=login)
         if not user:
             raise LoginNotExistsError(login=login)
         password_hash = self._password_service.hash_password(password)
--- src/core/use_cases/register_user.py
+++ src/core/use_cases/register_user.py
@@ -12,13 +12,13 @@ class RegisterUserUseCase:
         self._user_repo = user_service
         self._password_service = password_service
 
-    def register(self, first_name: str, last_name: str, login: str, password: str) -> User:
+    async def register(self, first_name: str, last_name: str, login: str, password: str) -> User:
         user = self._user_repo.find_by_login(login=login)
         if user:
             raise LoginAlreadyExistsError(login=login)
         self._validate_password(password)
         password_hash = self._password_service.hash_password(password)
-        return self._user_repo.create(
+        return await self._user_repo.create(
             first_name=first_name, last_name=last_name, login=login, password_hash=password_hash
         )
 
