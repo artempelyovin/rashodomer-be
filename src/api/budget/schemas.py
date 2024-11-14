4a4aa0a01c6aa2e51f6dabc95b408126841246f2
artempelyovin <artempelevin@list.ru>
2024-11-15 00:57:47 +0300

    feat: add `POST v1/budgets` endpoint

---
+++ src/api/budget/schemas.py
@@ -0,0 +1,28 @@
+from typing import Annotated
+
+from pydantic import UUID4, BaseModel, Field
+
+IdField = Field(..., description="Unique ID of the budget")
+NameField = Field(..., description="The name of the budget", examples=["Cash"])
+DescriptionField = Field(
+    ...,
+    description="The description of the budget",
+    examples=["A budget for tracking all cash transactions and managing daily expenses"],
+)
+AmountField = Field(..., ge=0, description="The amount of the budget", examples=[42200])
+UserIDField = Field(..., description="ID of user who created this budget")
+
+
+class CreateBudgetSchema(BaseModel):
+    name: str = NameField
+    description: str = DescriptionField
+    amount: float = AmountField
+    user_id: Annotated[str, UUID4] = UserIDField
+
+
+class BudgetSchema(BaseModel):
+    id: Annotated[str, UUID4] = IdField
+    name: str = NameField
+    description: str = DescriptionField
+    amount: float = AmountField
+    user_id: str = UserIDField
