4767ee13510e127ee372693198d95f96202b1b83
artempelevin <artempelevin@list.ru>
2025-08-12 11:25:00 +0300

    chore: run `make format`

--- src/ui/pages/user.py
+++ src/ui/pages/user.py
@@ -14,21 +14,21 @@ async def about_me(request: Request):
 
     with ui.row():
         ui.button(icon="arrow_back", on_click=ui.navigate.back)
-        ui.label("Обо мне").classes('')
+        ui.label("Обо мне").classes("")
     with ui.row():
         id_with_copy(user.id)
     with ui.row():
-        ui.label("Имя:").classes('text-sm')
+        ui.label("Имя:").classes("text-sm")
         ui.label(user.first_name)
     with ui.row():
-        ui.label("Фамилия:").classes('text-sm')
+        ui.label("Фамилия:").classes("text-sm")
         ui.label(user.last_name)
     with ui.row():
-        ui.label("Логин:").classes('text-sm')
+        ui.label("Логин:").classes("text-sm")
         ui.label(user.login)
     with ui.row():
-        ui.label("Дата регистрации:").classes('text-sm')
+        ui.label("Дата регистрации:").classes("text-sm")
         ui.label(datetime_to_text(user.created_at))
     with ui.row():
-        ui.label("Последний заход:").classes('text-sm')
+        ui.label("Последний заход:").classes("text-sm")
         ui.label(datetime_to_text(user.last_login))
